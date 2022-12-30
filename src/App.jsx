b2948dd76d5db1da1ff18221fa40238703060588
theraycore <raymundo.host@gmail.com>
2022-12-30 11:29:09 -0600

    Peticion Simple en React con Fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react"
+
+const CRYPTOCURRENCY_LIST_API_URL = 'https://api.coincap.io/v2/assets';
 
 function App() {
+
+  const [criptos, setCriptos] = useState();
+
+  useEffect(() => {
+    fetch(CRYPTOCURRENCY_LIST_API_URL)
+      .then(response => response.json())
+      .then(data => {
+        setCriptos(data.data);
+      })
+      .catch(() => {
+        console.error('Peticion Fallo');
+      })
+  }, [])
+
+  if (!criptos) return <span>Cargando...</span>
+
   return (
-    <h1>Hola mundo soy El Raycore!</h1>
+    <>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        {criptos.map(({id, name, priceUsd}) => {
+          return (
+            <li key={id}>
+              Nombre: {name} <br />
+              Precio: {priceUsd} <br />
+            </li>
+          )
+        })}
+      </ol>
+    </>
   )
 }
 
